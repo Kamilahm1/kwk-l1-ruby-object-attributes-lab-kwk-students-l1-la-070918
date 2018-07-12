12c1cd280416775f2a2d84c27d10120fc4074615
Kamilahm1 <kamilahmorgan1@gmail.com>
2018-07-12 22:51:55 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,13 @@
 class Person
-def initialize("name")
+def initialize("name", "job")
 @name = name
-
+@job = job
  end
 
 def name
   name = "Kevin"
   
+def name=(new_name)
+  name=new_name
  end
 end
