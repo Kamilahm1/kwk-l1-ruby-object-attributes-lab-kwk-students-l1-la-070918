e562fb8264f19e96cdfd075e1912293892693874
Kamilahm1 <kamilahmorgan1@gmail.com>
2018-07-12 22:54:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
 def initialize("dog_name" , "dogs_breeds")
-@name = dog_namename
+@name = dogs_name
 @breed = dogs_breeds
  end
 
-def name=()
+def name=(dogs_names)
   name = "Dill"
 
 def name=(new_name)
