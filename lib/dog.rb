783e25a27b064e090c6c620b36c2950f3e817b27
Kamilahm1 <kamilahmorgan1@gmail.com>
2018-07-12 23:02:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,4 @@
 class Dog
-def initialize("dog_name" , "dogs_breeds")
-@name = dogs_name
-@breed = dogs_breeds
- end
 
-def name=("dogs_names", "dogs_breeds" ="Mutt")
-  @name=dogs_names
-  @breed=dogs_breeds
-end
-
-def name
-  @name
- end
-def breed
-  @breed
 end
 end
